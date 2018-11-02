f5b50a2296e8107776fbbc4e99c2410a48dcee58
binnear <binnear@163.com>
2018-11-02 20:32:44 +0800

    cnzz

--- src/container/content/content.tsx
+++ src/container/content/content.tsx
@@ -5,15 +5,20 @@ const paint2 = require('public/images/paint2.svg');
 
 export default function Content() {
   return <div className={s.content}>
-      <span className={s.paint} style={{ background: `no-repeat 0% 44%/94% url(${paint1})` }}>
-        <i className="iconfont icon-blog"></i>
-        <a href="/blog/" >Blog</a>
+    <span className={s.paint} style={{ background: `no-repeat 0% 44%/94% url(${paint1})` }}>
+      <i className="iconfont icon-blog"></i>
+      <a
+        href="/blog/"
+        data-cnzz-event={JSON.stringify({ category: '首页', action: '进入我的博客', label: 'blog' })}
+      >Blog</a>
+    </span>
+    <span className={s.paint} style={{ background: `no-repeat 0% 30%/94% url(${paint2})` }}>
+      <i className="iconfont icon-github"></i>
+      <a
+        href="https://github.com/binnear/"
+        data-cnzz-event={JSON.stringify({ category: '首页', action: '进入我的github', label: 'github' })}
+      >Github</a>
 
-      </span>
-      <span className={s.paint} style={{ background: `no-repeat 0% 30%/94% url(${paint2})` }}>
-        <i className="iconfont icon-github"></i>
-        <a href="https://github.com/binnear/">Github</a>
-
-      </span>
-    </div>
+    </span>
+  </div>
 }
